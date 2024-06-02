d0d1a98af09115c488a2dfcb2a193d4d5e559fe4
Priyanka Shakya <100750349+priyankas9@users.noreply.github.com>
2024-06-02 12:02:32 +0545

    Update on rectangle java for question 3

--- Rectangle.java
+++ Rectangle.java
@@ -2,12 +2,12 @@ public class Rectangle {
     private double width;
     private double height;
 
+
     private static String color;
 
-
     public Rectangle() {
-        this.width = 8.0;
-        this.height = 8.0;
+        this.width = 1.0;
+        this.height = 1.0;
     }
 
     public Rectangle(double width, double height, String color) {
@@ -15,21 +15,26 @@ public class Rectangle {
         this.height = height;
         Rectangle.color = color;
     }
+
     public double getWidth() {
         return width;
     }
+
 
     public void setWidth(double width) {
         this.width = width;
     }
 
+
     public double getHeight() {
         return height;
     }
 
+
     public void setHeight(double height) {
         this.height = height;
     }
+
 
     public static String getColor() {
         return color;
@@ -40,25 +45,12 @@ public class Rectangle {
         Rectangle.color = color;
     }
 
+
     public double findArea() {
         return width * height;
     }
 
     public double findPerimeter() {
         return 2 * (width + height);
-    }
-
-    public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle(4.0, 5.0, "Blue");
-
-        System.out.println("Width: " + rect1.getWidth());
-        System.out.println("Height: " + rect1.getHeight());
-        System.out.println("Color: " + Rectangle.getColor());
-        System.out.println("Area: " + rect1.findArea());
-        System.out.println("Perimeter: " + rect1.findPerimeter());
-
-        Rectangle.setColor("Amber");
-
-        System.out.println("Updated Color: " + Rectangle.getColor());
     }
 }
