d670dd2851c548a297369aabd752c50027768086
Priyanka Shakya <100750349+priyankas9@users.noreply.github.com>
2024-06-18 14:13:08 +0545

    assignment 8 continue

---
+++ Discount.java
@@ -0,0 +1,18 @@
+public class Discount {
+    public static double getServiceDiscountRate(String membershipType) {
+        switch (membershipType) {
+            case "Premium":
+                return 0.2;
+            case "Gold":
+                return 0.15;
+            case "Silver":
+                return 0.1;
+            default:
+                return 0.0;
+        }
+    }
+
+    public static double getProductDiscountRate(String membershipType) {
+        return 0.1; // Flat 10% discount for all members on products
+    }
+}
