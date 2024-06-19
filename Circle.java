5e1861ddc18d265121b0717fa6fa78169f6738c5
Priyanka Shakya <100750349+priyankas9@users.noreply.github.com>
2024-06-19 10:40:40 +0545

    ass 1,q4.1

---
+++ Circle.java
@@ -0,0 +1,17 @@
+public class Circle extends GeometricObject {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{radius=" + radius + ", area=" + getArea() + "}";
+    }
+}
