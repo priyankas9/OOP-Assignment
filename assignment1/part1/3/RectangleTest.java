7cb53565abe4b6fd1a30e18d336a314d404db90b
Priyanka Shakya <100750349+priyankas9@users.noreply.github.com>
2024-08-09 12:54:23 +0545

    minor

--- assignment1/part1/3/RectangleTest.java
+++ assignment1/part1/3/RectangleTest.java
@@ -1,10 +1,10 @@
 public class RectangleTest {
     public static void main(String[] args) {
-        // Create two Rectangle objects
+     
         Rectangle rect1 = new Rectangle(5, 50, "Yellow");
         Rectangle rect2 = new Rectangle(5, 50, "Yellow");
 
-        // Display the properties of rect1
+  
         System.out.println("Rectangle 1:");
         System.out.println("Width: " + rect1.getWidth());
         System.out.println("Height: " + rect1.getHeight());
@@ -12,7 +12,6 @@ public class RectangleTest {
         System.out.println("Area: " + rect1.findArea());
         System.out.println("Perimeter: " + rect1.findPerimeter());
 
-        // Display the properties of rect2
         System.out.println("Rectangle 2:");
         System.out.println("Width: " + rect2.getWidth());
         System.out.println("Height: " + rect2.getHeight());
