138834a12cc290e12f3175c1b68fd533d124342b
Priyanka Shakya <100750349+priyankas9@users.noreply.github.com>
2024-08-09 12:53:28 +0545

    minor

--- assignmentt2/E/Account.java
+++ assignmentt2/E/Account.java
@@ -1,45 +1,39 @@
 public class Account {
-    // Private member variables
+  
     private String id;
     private String name;
     private int balance;
 
-    // Constructor with id and name
     public Account(String id, String name) {
         this.id = id;
         this.name = name;
         this.balance = 0;
     }
 
-    // Constructor with id, name, and balance
     public Account(String id, String name, int balance) {
         this.id = id;
         this.name = name;
         this.balance = balance;
     }
 
-    // Getter for ID
     public String getID() {
         return id;
     }
 
-    // Getter for name
     public String getName() {
         return name;
     }
 
-    // Getter for balance
     public int getBalance() {
         return balance;
     }
 
-    // Method to credit amount to the balance
+   
     public int credit(int amount) {
         balance += amount;
         return balance;
     }
 
-    // Method to debit amount from the balance
     public int debit(int amount) {
         if (amount <= balance) {
             balance -= amount;
@@ -49,7 +43,7 @@ public class Account {
         return balance;
     }
 
-    // Method to transfer amount to another account
+  
     public int transferTo(Account another, int amount) {
         if (amount <= balance) {
             this.debit(amount);
@@ -60,18 +54,16 @@ public class Account {
         return balance;
     }
 
-    // toString method
     @Override
     public String toString() {
         return "Account[id=" + id + ", name=" + name + ", balance=" + balance + "]";
     }
 
-    // Main method for testing
+
     public static void main(String[] args) {
         Account acc1 = new Account("A001", "John Doe", 1000);
         Account acc2 = new Account("A002", "Jane Smith", 500);
 
-        // Testing the Account class
         System.out.println(acc1);
         System.out.println(acc2);
 
